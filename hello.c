68158198fc726aa8b6c5932f346814db487a2c82
Tate Koziol <tkoziol2@huskers.unl.edu>
2021-08-26 16:14:05 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Tate Koziol
+ *         cbourke@cse.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tate Koziol\n");
+
+  return 0;
+}
